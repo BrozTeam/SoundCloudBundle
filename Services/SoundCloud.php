dd99e471f2d326b2ea5eafbcd0450a756a2648bb
Sandor Major <airborn22@gmail.com>
2015-06-19 18:11:59 +0200

    add getTrackById()

--- Services/SoundCloud.php
+++ Services/SoundCloud.php
@@ -2,14 +2,20 @@
 
 namespace Broz\SoundCloudBundle\Services;
 
+use Soundcloud\Exception\InvalidHttpResponseCodeException;
+use Soundcloud\Service as IseSoundCloud;
 
-class SoundCloud {
-    protected $clientId;
-    protected $clientSecret;
-
+class SoundCloud extends IseSoundCloud {
     public function __construct($config) {
-        $this->clientId = $config['client_id'];
-        $this->clientSecret = $config['client_secret'];
+        parent::__construct($config['client_id'], $config['client_secret']);
+    }
+
+    public function getTrackById($trackId) {
+        try {
+            return json_decode($this->get('tracks/' . $trackId), true);
+        }
+        catch (InvalidHttpResponseCodeException $e) {
+            throw new \RuntimeException(sprintf('Could not find track with id %s', $trackId), 0, $e);
+        }
     }
 }
-
